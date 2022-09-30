e85c69f4dfde4e17ce5e0d751fb235094c754434
Pavelkv94 <pavel.kv94@yandex.by>
2022-09-30 17:25:55 +0300

    fix i18n

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,18 @@ import i18n from 'i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import translationEN from "./locales/en/translation.json";
+import translationRU from "./locales/ru/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  ru: {
+    translation: translationRU
+  }
+};
+
 
 i18n
   // Подключение бэкенда i18next
@@ -21,7 +33,8 @@ i18n
     },
     interpolation: {
       escapeValue: false
-    }
+    },
+    resources
   })
 
 export default i18n;
